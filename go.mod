0b1709459ac4955cd449ec254161c48e44aee6e6
Joe Zou <yixian.zou@gmail.com>
2019-10-15 13:18:10 +0800

    Update test

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/zouyx/agollo_demo
 
-require (
-	github.com/cespare/xxhash v1.1.0 // indirect
-	github.com/coocood/freecache v1.1.0 // indirect
-	github.com/tevid/gohamcrest v1.1.1 // indirect
-	github.com/zouyx/agollo v1.9.0
-)
+require github.com/zouyx/agollo v1.9.0
--- main/check.go
+++ main/check.go
@@ -5,18 +5,21 @@ import (
 	"fmt"
 	"github.com/zouyx/agollo"
 	"net/http"
+	"strings"
 )
 
+var NamespaceName="application,product.joe"
+
 func main() {
-	agollo.InitCustomConfig(func () (*agollo.AppConfig, error) {
-		return &agollo.AppConfig{
-			AppId:         "testApplication_yang",
-			Cluster:       "dev",
-			Ip:            "http://106.12.25.204:8080",
-			NamespaceName: "application",
-		}, nil
-	})
-	go agollo.Start()
+	//agollo.InitCustomConfig(func () (*agollo.AppConfig, error) {
+	//	return &agollo.AppConfig{
+	//		AppId:         "testApplication_yang",
+	//		Cluster:       "dev",
+	//		Ip:            "http://106.12.25.204:8080",
+	//		NamespaceName: NamespaceName,
+	//	}, nil
+	//})
+	go agollo.StartWithLogger(&DefaultLogger{})
 
 	http.HandleFunc("/check",GetAllConfig)
 
@@ -24,8 +27,7 @@ func main() {
 }
 
 func GetAllConfig(rw http.ResponseWriter,req *http.Request)  {
-	config:=agollo.GetCurrentApolloConfig()
-	cache:=agollo.GetApolloConfigCache()
+	config:=agollo.GetCurrentApolloConfig()["application"]
 
 	var buffer bytes.Buffer
 	buffer.WriteString("<html>")
@@ -35,33 +37,70 @@ func GetAllConfig(rw http.ResponseWriter,req *http.Request)  {
 	if key=="" {
 		buffer.WriteString(fmt.Sprintf("AppId : %s  <br/>", config.AppId))
 		buffer.WriteString(fmt.Sprintf("Cluster : %s <br/>", config.Cluster))
-		buffer.WriteString(fmt.Sprintf("NamespaceName : %s <br/>", config.NamespaceName))
 		buffer.WriteString(fmt.Sprintf("ReleaseKey : %s <br/>", config.ReleaseKey))
 
-		buffer.WriteString("Configurations: <br/>")
-		it := cache.NewIterator()
-		for i := 0; i < int(cache.EntryCount()); i++ {
-			entry := it.Next()
-			if entry == nil {
-				continue
-			}
-			buffer.WriteString(fmt.Sprintf("key : %s , value : %s <br/>", string(entry.Key), string(entry.Value)))
-		}
-	}else{
-		if config!=nil&&cache.EntryCount()>0{
-			value,err:=cache.Get([]byte(key))
-
-			if err!=nil {
-				buffer.WriteString(fmt.Sprintf("get key : %s fail, error:%s <br/>", key,err.Error()))
-			}else{
-				buffer.WriteString(fmt.Sprintf("key : %s , value : %s <br/>", key, string(value)))
-			}
-		}else{
-			buffer.WriteString("no value in cache!")
+		namespaces:=strings.Split(NamespaceName,",")
+		for _, namespace := range namespaces {
+			writeConfig(&buffer,namespace)
 		}
 	}
 
 	buffer.WriteString("</html>")
 
 	rw.Write(buffer.Bytes())
+}
+
+func writeConfig(buffer *bytes.Buffer, namespace string) {
+	buffer.WriteString(fmt.Sprintf("NamespaceName : %s <br/>", namespace))
+	buffer.WriteString("Configurations: <br/>")
+	cache := agollo.GetConfigCache(namespace)
+	it := cache.NewIterator()
+	for i := 0; i < int(cache.EntryCount()); i++ {
+		entry := it.Next()
+		if entry == nil {
+			continue
+		}
+		buffer.WriteString(fmt.Sprintf("key : %s , value : %s <br/>", string(entry.Key), string(entry.Value)))
+	}
+}
+
+type DefaultLogger struct {
+
+}
+
+func (this *DefaultLogger)Debugf(format string, params ...interface{})  {
+	this.Debug(format,params)
+}
+
+func (this *DefaultLogger)Infof(format string, params ...interface{}) {
+	this.Debug(format,params)
+}
+
+
+func (this *DefaultLogger)Warnf(format string, params ...interface{}) error {
+	this.Debug(format,params)
+	return nil
+}
+
+func (this *DefaultLogger)Errorf(format string, params ...interface{}) error {
+	this.Debug(format,params)
+	return nil
+}
+
+
+func (this *DefaultLogger)Debug(v ...interface{}) {
+	fmt.Println(v)
+}
+func (this *DefaultLogger)Info(v ...interface{}){
+	this.Debug(v)
+}
+
+func (this *DefaultLogger)Warn(v ...interface{}) error{
+	this.Debug(v)
+	return nil
+}
+
+func (this *DefaultLogger)Error(v ...interface{}) error{
+	this.Debug(v)
+	return nil
 }
