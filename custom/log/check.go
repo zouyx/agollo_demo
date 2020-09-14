ebd94cf56fcaad644e57076f4fa6e6dd7cda1cfd
Joe Zou <yixian.zou@gmail.com>
2020-09-14 18:51:33 +0800

    update log demo

--- custom/log/check.go
+++ custom/log/check.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/cihub/seelog"
 	"github.com/zouyx/agollo/v4"
 	"github.com/zouyx/agollo/v4/env/config"
 )
@@ -16,7 +17,9 @@ func main() {
 		IsBackupConfig: false,
 		Secret:         "6ce3ff7e96a24335a9634fe9abca6d51",
 	}
-	agollo.SetLogger(&DefaultLogger{})
+
+	loggerInterface:=initSeeLog("seelog.xml")
+	agollo.SetLogger(&DefaultLogger{loggerInterface})
 
 	client,error:=agollo.StartWithConfig(func() (*config.AppConfig, error) {
 		return c, nil
@@ -36,6 +39,7 @@ func writeConfig(namespace string,client *agollo.Client) {
 }
 
 type DefaultLogger struct {
+	log seelog.LoggerInterface
 }
 
 func (this *DefaultLogger) Debugf(format string, params ...interface{}) {
@@ -55,7 +59,7 @@ func (this *DefaultLogger) Errorf(format string, params ...interface{}) {
 }
 
 func (this *DefaultLogger) Debug(v ...interface{}) {
-	fmt.Println(v)
+	this.log.Debug(v)
 }
 func (this *DefaultLogger) Info(v ...interface{}) {
 	this.Debug(v)
@@ -67,4 +71,19 @@ func (this *DefaultLogger) Warn(v ...interface{}) {
 
 func (this *DefaultLogger) Error(v ...interface{}) {
 	this.Debug(v)
+}
+
+func initSeeLog(configPath string) seelog.LoggerInterface {
+	logger, err := seelog.LoggerFromConfigAsFile(configPath)
+
+	//if error is happen change to default config.
+	if err != nil {
+		logger, err = seelog.LoggerFromConfigAsBytes([]byte("<seelog />"))
+	}
+
+	logger.SetAdditionalStackDepth(1)
+	seelog.ReplaceLogger(logger)
+	defer seelog.Flush()
+
+	return logger
 }
