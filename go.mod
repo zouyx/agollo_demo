44986d903cf5f4f5c609330937311f2b20f29420
Joe Zou <yixian.zou@gmail.com>
2020-09-14 22:54:58 +0800

    add hello world check

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/zouyx/agollo_demo
 
 require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
-	github.com/zouyx/agollo/v4 v4.0.0-20200914102923-3ec4472360b3
+	github.com/zouyx/agollo/v4 v4.0.0-20200914144959-37728f91efe4
 )
 
 go 1.13
--- helloworld/check.go
+++ helloworld/check.go
@@ -15,6 +15,7 @@ func main() {
 		IsBackupConfig: false,
 		Secret:         "6ce3ff7e96a24335a9634fe9abca6d51",
 	}
+	agollo.SetLogger(&DefaultLogger{})
 
 	client,err:=agollo.StartWithConfig(func() (*config.AppConfig, error) {
 		return c, nil
@@ -25,13 +26,53 @@ func main() {
 		panic(err)
 	}
 
-	writeConfig(c.NamespaceName,client)
+	checkKey(c.NamespaceName,client)
 }
 
-func writeConfig(namespace string,client *agollo.Client) {
+func checkKey(namespace string,client *agollo.Client) {
 	cache := client.GetConfigCache(namespace)
+	count:=0
 	cache.Range(func(key, value interface{}) bool {
-		fmt.Println("key : ", key, ", value :", value) 
+		fmt.Println("key : ", key, ", value :", value)
+		count++
 		return true
 	})
+	if count<1{
+		panic("config key can not be null")
+	}
+}
+
+
+type DefaultLogger struct {
+}
+
+func (this *DefaultLogger) Debugf(format string, params ...interface{}) {
+	this.Debug(format, params)
+}
+
+func (this *DefaultLogger) Infof(format string, params ...interface{}) {
+	this.Debug(format, params)
+}
+
+func (this *DefaultLogger) Warnf(format string, params ...interface{}) {
+	this.Debug(format, params)
+}
+
+func (this *DefaultLogger) Errorf(format string, params ...interface{}) {
+	this.Debug(format, params)
+}
+
+func (this *DefaultLogger) Debug(v ...interface{}) {
+	fmt.Println(v)
+}
+func (this *DefaultLogger) Info(v ...interface{}) {
+	this.Debug(v)
+}
+
+func (this *DefaultLogger) Warn(v ...interface{}) {
+	this.Debug(v)
+}
+
+func (this *DefaultLogger) Error(v ...interface{}) {
+	this.Debug(v)
 }
