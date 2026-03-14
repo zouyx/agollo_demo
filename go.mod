27d9cffbb153b5abcfabf4bfb878f750672f0f5b
Joe Zou <yixian.zou@gmail.com>
2026-03-14 18:22:46 +0800

    Fix variadic logger forwarding and stabilize module resolution

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
 )
 
+replace github.com/golang/protobuf => github.com/golang/protobuf v1.5.2
+
 go 1.13
--- web/check.go
+++ web/check.go
@@ -95,32 +95,32 @@ type DefaultLogger struct {
 }
 
 func (this *DefaultLogger) Debugf(format string, params ...interface{}) {
-	this.Debug(format, params)
+	this.Debug(append([]interface{}{format}, params...)...)
 }
 
 func (this *DefaultLogger) Infof(format string, params ...interface{}) {
-	this.Debug(format, params)
+	this.Debug(append([]interface{}{format}, params...)...)
 }
 
 func (this *DefaultLogger) Warnf(format string, params ...interface{}) {
-	this.Debug(format, params)
+	this.Debug(append([]interface{}{format}, params...)...)
 }
 
 func (this *DefaultLogger) Errorf(format string, params ...interface{}) {
-	this.Debug(format, params)
+	this.Debug(append([]interface{}{format}, params...)...)
 }
 
 func (this *DefaultLogger) Debug(v ...interface{}) {
-	fmt.Println(v)
+	fmt.Println(v...)
 }
 func (this *DefaultLogger) Info(v ...interface{}) {
-	this.Debug(v)
+	this.Debug(v...)
 }
 
 func (this *DefaultLogger) Warn(v ...interface{}) {
-	this.Debug(v)
+	this.Debug(v...)
 }
 
 func (this *DefaultLogger) Error(v ...interface{}) {
-	this.Debug(v)
+	this.Debug(v...)
 }
